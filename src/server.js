706412a2cfb848ae02a769387b4065b4704d654a
Vinícius de Morais <95432848+viniciusdmorais@users.noreply.github.com>
2026-05-05 11:40:49 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
@@ -11,6 +12,6 @@ app.get('/', (req, res) => {
     res.send('🚀 API rodando!');
 });
 
-app.listen(3000, () => {
-    console.log('🔥 Servidor rodando em http://localhost:3000');
+app.listen(PORT, () => {
+    console.log(`🔥 Servidor rodando na porta ${PORT}`);
 });
